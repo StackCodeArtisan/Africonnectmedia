9cc4d3906514111ab146cd4a5befe4ce450dbf01
StackCodeArtisan <joshuaollaoluwa@gmail.com>
2026-01-26 11:44:25 +0100

    remove teams page and teams preview from about page

--- app.py
+++ app.py
@@ -36,10 +36,6 @@ def about():
 def services():
     return render_template('services.html')
 
-@app.route('/team')
-def team():
-    return render_template('team.html')
-
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
